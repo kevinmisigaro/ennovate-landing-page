0c593ca63ee386e35670bde787c29f69390aaac2
Headrick <104570437+headrickleonard@users.noreply.github.com>
2024-03-28 16:39:00 +0300

    modified the newsletter articles to be downloadable

--- src/pages/NewsLetter.jsx
+++ src/pages/NewsLetter.jsx
@@ -9,6 +9,9 @@ import image3 from "../assets/image3.jpg";
 import Carousel from "../components/Carousel";
 import Heading from "../components/Shared/Heading";
 import HighLightsCard from "../components/HighLightsCard";
+import doc1 from "../assets/docs/annual.pdf"
+import doc2 from "../assets/docs/fintech.pdf"
+import doc3 from "../assets/docs/food.pdf"
 
 const SubHero = () => {
   return (
@@ -61,12 +64,19 @@ function NewsLetter() {
       <div className="container mx-auto w-9/12 2xl:w-7/12 lg:flex lg:items-center lg:justify-center">
         <HighLights sectionTitle={"Ennovate News"} />
       </div>
-     <HighLightsCard/>
+    
       <div className="container mx-auto w-9/12 2xl:w-7/12 lg:flex lg:items-center lg:justify-center">
         <HighLights sectionTitle={"NewsLetter"} />
       </div>
-      <div className="container mx-auto w-9/12 2xl:w-7/12 lg:flex lg:items-center lg:justify-center">
+      {/* <div className="container mx-auto w-9/12 2xl:w-7/12 lg:flex lg:items-center lg:justify-center">
         <HighLights sectionTitle={"Articles"} />
+      </div> */}
+      <div>
+        <h1 className="text-yellow-600 font-bold text-3xl lg:text-4xl md:my-4 text-center">Articles</h1>
+       <HighLightsCard docLink={doc1}/>
+       <HighLightsCard docLink={doc2}/>
+       <HighLightsCard docLink={doc3}/>
+
       </div>
       {/* <Media/> */}
       <Heading title={'Media'}/>
