13af0d05ecf13560e4c24cf8774d118e3219e652
Kevin Misigaro <kevin@databalk.nu>
2024-11-06 09:17:43 +0200

    added item

--- src/pages/Careers.jsx
+++ src/pages/Careers.jsx
@@ -1,22 +1,8 @@
-import { Skeleton } from "@/components/ui/skeleton";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import React from "react";
-import developp from "../assets/blogs/developp.jpg";
-import pretty from "../assets/blogs/meeting.jpg";
-import wahuni from "../assets/blogs/wahuni.jpg";
-import ruge from "../assets/blogs/ruge.jpg"
 import fintech from "../assets/blogs/Fintech.jpg"
 import doc1 from "../assets/docs/fintech.pdf";
-import doc3 from "../assets/docs/press.pdf";
-import doc2 from "../assets/docs/ruge.pdf";
-import image1 from "../assets/image1.jpg";
-import image2 from "../assets/image2.jpg";
-import image3 from "../assets/image3.jpg";
 import hero from "../assets/news_hero.jpg";
-import Carousel from "../components/Carousel";
 import HighLightsCard from "../components/HighLightsCard";
-import HighLights from "../components/Highlights";
-import Heading from "../components/Shared/Heading";
 import Footer from "./../components/Footer";
 import NavBar2 from "./../components/NavBar2";
 
@@ -28,59 +14,20 @@ const SubHero = () => {
     </div>
   );
 };
-const Media = () => {
-  const logos = [image1, image1, image1, image1];
-  return (
-    <div class="w-full inline-flex flex-nowrap bg-red-500 py-12 overflow-hidden [mask-image:_linear-gradient(to_right,transparent_1,_white_128px,_white_calc(100%-200px),transparent_100%)]">
-      <ul class="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll">
-        {logos.map((logo, index) => (
-          <img
-            key={index}
-            src={logo}
-            alt=""
-            className="h-[12em] w-[12em] object-contain my-8 sm:h-16 sm:w-[13em] mx-4 md:my-8  xl:scale-125 xl:mx-20 aspect-auto hover:-hue-rotate-15 transition-all duration-200 hover:scale-110 hover:cursor-pointer"
-          />
-        ))}
-      </ul>
-    </div>
-  );
-};
-export function SkeletonCard() {
-  return (
-    <div className="flex flex-col space-y-3 my-2 md:my-0">
-      <Skeleton className="h-[155px] w-[250px]  rounded-xl xl:w-[320px]" />
-      <div className="space-y-2">
-        <Skeleton className="h-4 w-[250px] xl:w-[320px]" />
-        <Skeleton className="h-4 w-[210px] xl:w-[280px]" />
-      </div>
-    </div>
-  );
-}
+
 const Articles = () => {
-  const logos = [image1, image2, image3, image1];
   const docs = [
     {
       id: crypto.randomUUID(),
-      title: "How Fintechs are Revolutionizing Transactions in Tanzania",
+      title: "Agri-Tech Innovation Fellowship: Apply Now!",
       attach: doc1,
       thumbnail: fintech,
-      desc: "Having personally witnessed the emergence and rise of fintech services in Tanzania and interacting with most of the solutions firsthand on a daily basis gives me so much hope for the potential fintech holds for harmonising and democratising access to financial services",
-    },
-    {
-      id: crypto.randomUUID(),
-      title: "Ennovate Ventures X Ruge Mutahaba",
-      attach: doc2,
-      thumbnail: ruge,
-      desc: "Ennovate Ventures and Ruge Mutahaba Foundation Join Forces to Empower Tanzania Youth.",
-    },
-    {
-      id: crypto.randomUUID(),
-      title: "Press Release_develoPPP Ventures Support",
-      attach: doc3,
-      thumbnail: developp,
-      desc: "Dar es Salaam, Tanzania, May 15th 2024, DEG Impulse announces the launch of a new call for applications for the develoPPP Ventures program, designed to support start-ups with €100,000 in grant financing and technical assistance to fuel business growth in Tanzania.",
-    },
+      desc: `
+      We&apos;re seeking six passionate Agri-Tech Innovation Fellows (3 Tanzanians, 3 Ugandans) to join a one-year, full-time fellowship aimed at transforming the agricultural sector through sustainable ventures. Apply by November 11, 2024, and be part of this impactful journey!
+      `
+      },
   ];
+
   return (
     <div>
       <h1 className="text-yellow-600 font-bold text-3xl lg:text-4xl md:my-4 text-center">
@@ -100,30 +47,6 @@ const Articles = () => {
 };
 
 function NewsLetter() {
-  const logos = [image1, image2, image3, image1];
-  const docs = [
-    {
-      id: crypto.randomUUID(),
-      title: "How Fintechs are Revolutionizing Transactions in Tanzania",
-      attach: doc1,
-      thumbnail: wahuni,
-      desc: "Having personally witnessed the emergence and rise of fintech services in Tanzania and interacting with most of the solutions firsthand on a daily basis gives me so much hope for the potential fintech holds for harmonising and democratising access to financial services",
-    },
-    {
-      id: crypto.randomUUID(),
-      title: "Ennovate Ventures X Ruge Mutahaba",
-      attach: doc2,
-      thumbnail: pretty,
-      desc: "Ennovate Ventures and Ruge Mutahaba Foundation Join Forces to Empower Tanzania Youth.",
-    },
-    {
-      id: crypto.randomUUID(),
-      title: "Press Release_develoPPP Ventures Support",
-      attach: doc3,
-      thumbnail: developp,
-      desc: "Dar es Salaam, Tanzania, May 15th 2024, DEG Impulse announces the launch of a new call for applications for the develoPPP Ventures program, designed to support start-ups with €100,000 in grant financing and technical assistance to fuel business growth in Tanzania.",
-    },
-  ];
   return (
     <div className="bg-black w-full h-full">
       <NavBar2 />
