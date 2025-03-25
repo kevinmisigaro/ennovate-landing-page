32ba67b0c2b7c038683dfae43b181cb9b1056273
Kevin Misigaro <kevin@databalk.nu>
2025-03-25 16:17:47 +0300

    added golden border

--- src/pages/NewsLetter.jsx
+++ src/pages/NewsLetter.jsx
@@ -239,7 +239,7 @@ function NewsLetter() {
       <News />
       <Heading title={"Innovate for Food Security"} />
       <div className="flex lg:items-center lg:justify-center mb-10">
-        <a href="/agribusiness">
+        <a href="/agribusiness" className="hover:border-yellow-500 hover:border-yellow-600 hover:box-border cursor-pointer">
           <img
             src={agri}
             className="w-[80vw] max-h-60 md:max-h-80 md:object-center object-cover"
