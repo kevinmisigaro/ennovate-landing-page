76d15d92489c9d26db1be5479a973ce6f3697302
Kevin Misigaro <kevin@databalk.nu>
2025-07-17 08:05:12 +0300

    changes on highlights

--- src/pages/NewsLetter.jsx
+++ src/pages/NewsLetter.jsx
@@ -106,19 +106,19 @@ const ArticleHighlights = () => {
       unbanked SMEs qualify and access business capital seamlessly. Swahilies is addressing a
       critical gap in Africas SME ecosystem by digitizing business operations`,
     },
-    {
-      id: crypto.randomUUID(),
-      title:
-        "develoPPP Ventures Supports Tanzanian Start-ups with non-dilutive capital",
-      attach:
-        "https://docs.google.com/document/d/1epcRNWnd9hxY8XI65k2oALw55UuRJF67/edit?usp=sharing&ouid=107996262346116417988&rtpof=true&sd=true",
-      thumbnail: Img2,
-      desc: `Through a matching-funds approach,
-      the German Federal Ministry for Economic Cooperation and
-      Development (BMZ) offers non-dilutive capital of up to
-      100,000 euros to co-finance strategic investments aimed at
-      scaling these impactful businesses.`,
-    },
+    // {
+    //   id: crypto.randomUUID(),
+    //   title:
+    //     "develoPPP Ventures Supports Tanzanian Start-ups with non-dilutive capital",
+    //   attach:
+    //     "https://docs.google.com/document/d/1epcRNWnd9hxY8XI65k2oALw55UuRJF67/edit?usp=sharing&ouid=107996262346116417988&rtpof=true&sd=true",
+    //   thumbnail: Img2,
+    //   desc: `Through a matching-funds approach,
+    //   the German Federal Ministry for Economic Cooperation and
+    //   Development (BMZ) offers non-dilutive capital of up to
+    //   100,000 euros to co-finance strategic investments aimed at
+    //   scaling these impactful businesses.`,
+    // },
   ];
   return (
     <div>
