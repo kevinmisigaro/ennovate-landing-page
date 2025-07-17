60a340a720fdc9852b3d19be746a05f12e2d84ae
Kevin Misigaro <kevin@databalk.nu>
2025-07-17 17:09:20 +0300

    added on highlights page

--- src/pages/Highlights.jsx
+++ src/pages/Highlights.jsx
@@ -8,6 +8,8 @@ import Img4 from "../assets/blogs/swahilies-cofunders.jpg";
 import Img5 from "../assets/blogs/ESEA Norad_All ESOs.png";
 import doc5 from "../assets/docs/ESEA 2025_Norad.pdf";
 import FollowupCard from "@/components/FollowupCard";
+import Img6 from "../assets/blogs/blog-4.jpg";
+import Img7 from "../assets/blogs/PHOTO-2025-07-17-16-55-56.jpg"
 
 function Highlights() {
   const SubHero = () => {
@@ -21,16 +23,20 @@ function Highlights() {
 
   const ArticleHighlights = () => {
     const docs = [
-      // {
-      //   id: crypto.randomUUID(),
-      //   title:
-      //     "Village Capital and Local ESOs Launch Accelerator for Sustainable Startups in Africa",
-      //   thumbnail: Img5,
-      //   attach: doc5,
-      //   desc: `Village Capital, in partnership with Norway, through Norad
-      //   – the Norwegian Agency for Development Cooperation, has launched the second phase of the
-      //   Empowering Sustainable Entrepreneurship Africa initiative.`,
-      // },
+      {
+        id: crypto.randomUUID(),
+        desc: "Tanzanian Start-ups Invited to Benefit from €100,000 (TSh282million) Funding Opportunity through the develoPPP Ventures Programme",
+        title: "Startups invited to benefit €100,000",
+        thumbnail: Img6,
+        attach: "https://drive.google.com/file/d/1aLBUTXpMnnjScShwMYzhxeX6uaZJiEiE/view?usp=sharing"
+      },
+      {
+        id: crypto.randomUUID(),
+        attach: "https://drive.google.com/file/d/1vkJqTaWVtz-Qk4jcYXx4CHeGFUOXYIoG/view?usp=sharing",
+        thumbnail: Img7,
+        title: "Ennovate Ventures Named ESO Venture Partner for Africa Ecosystem Catalysts Facility",
+        desc: "Ennovate Ventures Named ESO Venture Partner for Africa Ecosystem Catalysts Facility to Strengthen Early-Stage Entrepreneurship in Tanzania."
+      },
       {
         id: crypto.randomUUID(),
         title:
@@ -60,13 +66,13 @@ function Highlights() {
         <h1 className="text-yellow-600 font-bold text-3xl lg:text-4xl md:my-4 text-center">
           Highlights
         </h1>
-        <FollowupCard
+        {/* <FollowupCard
           description={`We are happy to announce our investment in Swahilies, a fintech startup that is helping
         unbanked SMEs qualify and access business capital seamlessly. Swahilies is addressing a
         critical gap in Africas SME ecosystem by digitizing business operations`}
           heading={`Village Capital and Local ESOs Launch Accelerator for Sustainable Startups in Africa`}
           avatar={Img5}
-        />
+        /> */}
         {docs.map((doc, index) => (
           <HighLightsCard
             docLink={doc.attach}
