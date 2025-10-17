69335b9b498b841b0c022bfeec11f1f36f663432
Kevin Misigaro <kevin@databalk.nu>
2025-10-17 15:33:41 +0300

    returned back white background

--- src/components/AnchorFirmsPartners.jsx
+++ src/components/AnchorFirmsPartners.jsx
@@ -2,7 +2,7 @@ import { anchorFirmsLogos } from '@/utils'
 
 function AnchorFirmsPartners() {
   return (
-    <div class="w-full inline-flex flex-nowrap py-12 overflow-hidden">
+    <div class="w-full inline-flex flex-nowrap bg-white py-12 overflow-hidden">
     <ul class="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none">
       {anchorFirmsLogos.map((logo, index) => (
         <img
