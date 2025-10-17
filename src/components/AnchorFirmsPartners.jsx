882eeb544e48e42a07e80679c682a403509569fe
Kevin Misigaro <kevin@databalk.nu>
2025-10-17 15:32:34 +0300

    removed white background

--- src/components/AnchorFirmsPartners.jsx
+++ src/components/AnchorFirmsPartners.jsx
@@ -1,9 +1,8 @@
 import { anchorFirmsLogos } from '@/utils'
-import React from 'react'
 
 function AnchorFirmsPartners() {
   return (
-    <div class="w-full inline-flex flex-nowrap bg-white py-12 overflow-hidden">
+    <div class="w-full inline-flex flex-nowrap py-12 overflow-hidden">
     <ul class="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none">
       {anchorFirmsLogos.map((logo, index) => (
         <img
