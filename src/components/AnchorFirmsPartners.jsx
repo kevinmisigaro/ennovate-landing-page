a120d39c73945c8f93fe53d9a28ace70731fc0b0
Kevin Misigaro <kevin@databalk.nu>
2025-10-17 15:27:41 +0300

    remove white background color

--- src/components/AnchorFirmsPartners.jsx
+++ src/components/AnchorFirmsPartners.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 function AnchorFirmsPartners() {
   return (
-    <div class="w-full inline-flex flex-nowrap bg-white py-12 overflow-hidden [mask-image:_linear-gradient(to_right,transparent_1,_white_128px,_white_calc(100%-200px),transparent_100%)]">
+    <div class="w-full inline-flex flex-nowrap bg-white py-12 overflow-hidden">
     <ul class="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll">
       {anchorFirmsLogos.map((logo, index) => (
         <img
